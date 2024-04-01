f97dbf6a5a82b7d9d2136c7225bec172e65e10f0
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2024-04-01 13:49:33 +0000

    Add railsを表示できるように

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,4 +67,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "9a3c1e1528bc491bbf26b7e39abb67f0.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
